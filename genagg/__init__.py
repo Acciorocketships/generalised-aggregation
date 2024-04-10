d3bf08efa68c3154e173e12c565f2632a815caa6
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-04-10 11:14:24 +0200

    amend

--- genagg/__init__.py
+++ genagg/__init__.py
@@ -1,5 +1,5 @@
-from .MLPAutoencoder import MLPAutoencoder
-from .genagg import GenAgg
-from .inn.InvertibleNN import InvertibleNN
+from genagg.inn.InvertibleNN import InvertibleNN
+from genagg.MLPAutoencoder import MLPAutoencoder
+from genagg.genagg import GenAgg
 
 __all__ = ["GenAgg", "InvertibleNN", "MLPAutoencoder"]
